7e7c04918bda708a4defd4d9274d4b383131907a
Arenas <jparenas@espol.edu.ec>
2025-07-26 19:07:56 -0500

    Project Test
    asd123

---
+++ funciones.py
@@ -0,0 +1,24 @@
+import time
+
+
+def mostrar_menu():
+    print("\n¿Qué deseas hacer?")
+    print("1️⃣  Mostrar todos los clientes")
+    print("2️⃣  Mostrar todas las rentas")
+    print("3️⃣  Mostrar los videojuegos disponibles")
+    print("0️⃣  Salir")
+    print("-" * 50)
+
+def mostrar_clientes():
+    print("\n👥 Lista de Clientes Registrados:")
+
+    time.sleep(1.5)
+
+def mostrar_rentas():
+    print("\n🧾 Rentas Actuales:")
+    time.sleep(1.5)
+
+def mostrar_videojuegos():
+    print("\n🎲 Videojuegos Disponibles:")
+
+    time.sleep(1.5)
---
+++ main.py
@@ -0,0 +1,28 @@
+from funciones import *
+while True:
+    print("=" * 50)
+    print("🎮  SRVP - Sistema de Rentas de Videojuegos y PS Plus  🎮".center(50))
+    print("=" * 50)
+    
+    print("\n¿Qué deseas hacer?")
+    print("1. Mostrar todos los clientes")
+    print("2. Mostrar todas las rentas")
+    print("3. Mostrar los videojuegos disponibles")
+    print("0. Salir")
+    print("-" * 50)
+
+    opcion = input("Selecciona una opción: ")
+
+    if opcion == "1":
+        mostrar_clientes()
+    elif opcion == "2":
+        mostrar_rentas()
+    elif opcion == "3":
+        mostrar_videojuegos()
+    elif opcion == "0":
+        print("\n👋 ¡Gracias por usar SRVP! Hasta pronto.")
+        break
+    else:
+        print("❌ Opción inválida. Intenta de nuevo.")
+
+    input("\nPresiona Enter para continuar...")
