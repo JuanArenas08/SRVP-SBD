37fcbfff576d68e04ddffa1319e0111b71354c64
pablochacon-hub <pablochacon028@gmail.com>
2025-08-07 00:22:28 -0500

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -3,7 +3,7 @@ import mysql.connector
 def obtener_conexion():
     return mysql.connector.connect(
         user = 'root',       # ingrese nombre del usuario
-        password = 'asdasd',   # ingrese contraseña de su servidor
+        password = 'admin',   # ingrese contraseña de su servidor
         host = 'localhost',       # ingrese nombre del servidor
         database = 'srvp',   # ingrese nombre de la base de datos
         port = 3306        # ingrese el puerto, por defecto es el 3306
